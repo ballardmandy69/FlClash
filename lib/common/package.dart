fb179dc7d8f8f673c2fccfe567f5483beed55c4a
ballardmandy69 <149659216+ballardmandy69@users.noreply.github.com>
2026-03-20 20:31:54 +0800

    Simplify ua extension in PackageInfoExtension

--- lib/common/package.dart
+++ lib/common/package.dart
@@ -5,9 +5,5 @@ import 'package:package_info_plus/package_info_plus.dart';
 import 'common.dart';
 
 extension PackageInfoExtension on PackageInfo {
-  String get ua => [
-        '$appName/v$version',
-        'clash-verge',
-        'Platform/${Platform.operatingSystem}',
-      ].join(' ');
+  String get ua => 'YeshaFlFlow';
 }
